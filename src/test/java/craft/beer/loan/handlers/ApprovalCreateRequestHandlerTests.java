80386b006aac8927ad70546fd5b1c374959e66fa
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-10-19 16:31:44 +0300

    tests

--- src/test/java/craft/beer/loan/handlers/ApprovalCreateRequestHandlerTests.java
+++ src/test/java/craft/beer/loan/handlers/ApprovalCreateRequestHandlerTests.java
@@ -1,7 +1,10 @@
 package craft.beer.loan.handlers;
 
 import craft.beer.loan.controller.requests.ApprovalCreateRequest;
+import craft.beer.loan.controller.responses.ApprovalCreateResponse;
 import craft.beer.loan.data.ILoanRepository;
+import craft.beer.loan.data.entities.ApprovalRequestEntity;
+import craft.beer.loan.handlers.async.AsyncPing;
 import craft.beer.loan.mappers.ApprovalMapper;
 import craft.beer.loan.services.ILoanValidatorService;
 import org.junit.jupiter.api.Test;
@@ -9,7 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class ApprovalCreateRequestHandlerTests {
@@ -27,10 +33,25 @@ public class ApprovalCreateRequestHandlerTests {
     private ApprovalMapper mapper;
 
     @Test
-    public void createRequest() {
+    public void handle_createRequestSuccess() {
+        var request = new ApprovalCreateRequest();
+        var requestEntity = new ApprovalRequestEntity();
+        var response = new ApprovalCreateResponse();
 
-        //handler.handle(new ApprovalCreateRequest());
+        when(mapper.requestToEntity(request)).thenReturn(requestEntity);
+        when(mapper.entityToResponse(requestEntity)).thenReturn(response);
 
-        assertTrue(true);
+        var result = handler.handle(request);
+
+        verify(mapper, times(1))
+                .requestToEntity(request);
+        verify(validationService, times(1))
+                .validateApprovalCreateRequest(requestEntity);
+        verify(repository, times(1))
+                .createApprovalRequest(requestEntity);
+        verify(mapper, times(1))
+                .entityToResponse(requestEntity);
+
+        assertEquals(response, result);
     }
 }
