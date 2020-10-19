9bd1637e6a0eac3cdc6b00049038df99dfc1a017
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-10-19 13:11:33 +0300

    apt dep removed

--- src/test/java/craft/beer/loan/services/LoanValidatorServiceTests.java
+++ src/test/java/craft/beer/loan/services/LoanValidatorServiceTests.java
@@ -2,6 +2,7 @@ package craft.beer.loan.services;
 
 import com.github.javafaker.Faker;
 import craft.beer.loan.controller.exceptions.LoanException;
+import craft.beer.loan.controller.requests.ApprovalUpdateRequest;
 import craft.beer.loan.data.ILoanRepository;
 import craft.beer.loan.data.entities.ApprovalRequestEntity;
 import org.junit.jupiter.api.Assertions;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -24,15 +26,38 @@ public class LoanValidatorServiceTests {
     private final Faker faker = new Faker();
 
     @Test
-    public void requestAlreadyCreatedException() {
+    public void validateApprovalCreateRequest_approvalRequestAlreadyCreatedLoanException() {
+        // arrange
         var customerId = faker.name().username();
         var approvalEntity = new ApprovalRequestEntity();
         approvalEntity.setCustomerId(customerId);
 
         when(repository.isLoanRequestCreated(customerId)).thenReturn(true);
 
-        Assertions.assertThrows(
+        // act
+        Throwable exceptionThatWasThrown = Assertions.assertThrows(
                 LoanException.class,
                 () -> validatorService.validateApprovalCreateRequest(approvalEntity));
+
+        // assert
+        assertEquals(exceptionThatWasThrown.getMessage(), "Loan request already created");
+    }
+
+    @Test
+    public void validateApprovalUpdateRequest_approvalRequestNotCreatedLoanException() {
+        // arrange
+        var customerId = faker.name().username();
+        var approvalEntity = new ApprovalUpdateRequest();
+        approvalEntity.setCustomerId(customerId);
+
+        when(repository.isLoanRequestCreated(customerId)).thenReturn(false);
+
+        // act
+        Throwable exceptionThatWasThrown = Assertions.assertThrows(
+                LoanException.class,
+                () -> validatorService.validateApprovalUpdateRequest(approvalEntity));
+
+        // assert
+        assertEquals(exceptionThatWasThrown.getMessage(), "Loan request is not created");
     }
 }
