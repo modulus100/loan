a9c0d64527dd90e6a005a9e6c2f8b80b468e03a5
Aleksandr Madisson <aleksandr.madisson@gmail.com>
2020-10-19 19:03:51 +0300

    tests

--- src/main/java/craft/beer/loan/controller/responses/ApproveUpdateResponse.java
+++ src/main/java/craft/beer/loan/controller/responses/ApproveUpdateResponse.java
@@ -3,8 +3,6 @@ package craft.beer.loan.controller.responses;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.Objects;
-
 @AllArgsConstructor
 public class ApproveUpdateResponse {
 
